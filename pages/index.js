9a2e48813d79f73884d522520ea1a07ca890c8e4
Mahmut <mahmutaktas@marun.edu.tr>
2021-02-13 17:23:53 +0300

    last commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,118 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { Component } from "react";
+import { MDBTable, MDBTableBody, MDBTableHead } from "mdbreact";
+import io from "socket.io-client";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+let endPoint = "http://127.0.0.1:5000";
+let socket = io.connect(`${endPoint}`);
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+class Home extends Component {
+  state = {
+    messages: {},
+    message: "",
+    counter: 0,
+    isDrowsy1: false,
+    isDrowsy2: false
+  };
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+  componentDidMount = async () => {
+    if (this.state.counter === 0) {
+      socket.emit("message", "message");
+      await this.setState({ counter: 1 });
+    }
+    socket.on("message", (msg) => {
+      console.log(msg.EARLYDETECTION)
+      console.log(typeof msg.EARLYDETECTION)
+      this.setState({
+        messages: msg,
+      });
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+      if (String(msg.EARLYDETECTION) !== "CALIB"){
+        if(Number(msg.EARLYDETECTION) < 0.53 ){
+          this.setState({isDrowsy2: true})
+        }else{
+          this.setState({isDrowsy2: false})
+        }
+      }
+      if (String(msg.DROWSINESS) !== "CALIB"){
+        if(Number(msg.DROWSINESS) == 1 ){
+          this.setState({isDrowsy1: true})
+        }else{
+          this.setState({isDrowsy1: true})
+        }
+      }
+    });
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+    console.log(this.state.messages);
+  };
+  
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+  render() {
+    return (
+      <div className="app">
+        <div className="drowsiness">
+          <h2 className="status">
+            <span style={{verticalAlign: "middle"}} >DROWSINESS STATUS: </span><span style={{verticalAlign: "middle"}} className={this.state.isDrowsy1 === true ? "drowsiness-text" : "awake-text"}>{String(this.state.messages.DROWSINESS) !== "CALIB" ? this.state.isDrowsy1 ? "DROWSY": "AWAKE" : "CALIBRATING..."}</span>
+          </h2>
+          <h2 className="status">
+            <span style={{verticalAlign: "middle"}} >EARLY DETECTION: </span><span style={{verticalAlign: "middle"}} className={this.state.isDrowsy2 === true ? "drowsiness-text" : "awake-text"}>{String(this.state.messages.EARLYDETECTION) !== "CALIB" ? this.state.isDrowsy2 ? "WARNING": "OK.": "CALIBRATING..."}</span>
+          </h2>
         </div>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+        <div className="main">
+          <div>
+            <img
+              src={"http://127.0.0.1:5000/video_feed"}
+              alt="logo"
+              className="video-frame"
+            />
+          </div>
+
+          <div className="info">
+            <div className="tables">
+              <MDBTable className="info-table" hover>
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Value</th>
+                  </tr>
+                </thead>
+                <tbody className="table-body">
+                  <tr>
+                    <td>Eye Aspect Ratio</td>
+                    <td>{Number(this.state.messages.EAR).toFixed(2)}</td>
+                  </tr>
+                  <tr>
+                    <td>Percantage of Eye Closure</td>
+                    <td>{Number(this.state.messages.PERCLOS).toFixed(2)}</td>
+                  </tr>
+                  <tr>
+                    <td>Mouth Aspect Ratio</td>
+                    <td>{Number(this.state.messages.MAR).toFixed(2)}</td>
+                  </tr>
+                  <tr>
+                    <td>Mouth Over Eye</td>
+                    <td>{Number(this.state.messages.MOE).toFixed(2)}</td>
+                  </tr>
+                  <tr>
+                    <td>Eye Circularity</td>
+                    <td>{Number(this.state.messages.EC).toFixed(2)}</td>
+                  </tr>
+                  <tr>
+                    <td>Level of Brow</td>
+                    <td>{Number(this.state.messages.LEB).toFixed(2)}</td>
+                  </tr>
+                  <tr>
+                    <td>Size of Pupil</td>
+                    <td>{Number(this.state.messages.SOP).toFixed(2)}</td>
+                  </tr>
+                </tbody>
+              </MDBTable>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default Home;
